2a7891d6bb301a660cd021cd757ec9e765e5c995
Archit <82609753+orcheat@users.noreply.github.com>
2023-05-11 00:21:44 +0530

    Add files via upload

---
+++ DiceGame/index.js
@@ -0,0 +1,22 @@
+function startRoll()
+{var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var image1="images/dice"+randomNumber1+".png";
+var image2="images/dice"+randomNumber2+".png";
+document.querySelector(".img1").setAttribute("src",image1);
+document.querySelector(".img2").setAttribute("src",image2);
+
+if(randomNumber1>randomNumber2)
+{
+    document.querySelector("h2").innerHTML="🔥Player 1 Wins!";
+}
+else if(randomNumber1<randomNumber2)
+{
+    document.querySelector("h2").innerHTML="🔥Player 2 Wins!";
+}
+else
+{
+    document.querySelector("h2").innerHTML="Draw!";
+}
+}
+document.onclick=function() {startRoll()}
